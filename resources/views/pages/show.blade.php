db19bcbc78f4e8a76849dd7fa0036ff27b92780a
Alexandre <alex.sampedrolerida@gmail.com>
2020-09-22 15:03:29 +0200

    Intégration du template

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -4,12 +4,33 @@
   $page: OBJ(titre, sousTitre, texte, titreMenu, image, tri)
 --}}
 
+@extends('template.index')
+  @section('content')
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url( {{asset ('assets/img/' . $page->image) }})">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>{{$page->titre}}</h1>
+              <span class="subheading">{{$page->sousTitre}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
 
-<h1>{{ $page -> titre }}</h1>
+    <!-- Textes -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="clearfix">
+            {!! html_entity_decode($page->texte) !!}
+          </div>
 
-<h2>{{ $page -> sousTitre }}</h2>
+          {{-- Ici viennent les contenus complémentaires (posts, formulaire, ...) --}}
 
-
-<div class="">
-  {!! $page -> texte !!}
-</div>
+        </div>
+      </div>
+    </div>
+  @endsection
---
+++ resources/views/template/partials/_scripts.blade.php
@@ -0,0 +1,10 @@
+{{--
+  ./resources/views/template/partials/_scripts.blade.php
+--}}
+
+<script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/popper/popper.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.j') }}"></script>
+
+<!-- Custom scripts for this template -->
+<script src="{{ asset('assets/js/clean-blog.min.js') }}"></script>
